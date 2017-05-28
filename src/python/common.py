926178c19caea53cb3215fc81503e440f31577ef
Inderpreet Singh <ipsingh06@gmail.com>
2017-05-28 13:36:38 -0400

    Loading config and patterns file

--- src/python/common.py
+++ src/python/common.py
@@ -4,6 +4,19 @@ import logging
 import threading
 import time
 from abc import ABC, abstractmethod
+from typing import Dict, List
+
+
+# Custom types
+Config = Dict[str, Dict[str, str]]
+Patterns = List[str]
+
+
+class PylftpError(Exception):
+    """
+    Exception indicating an error
+    """
+    pass
 
 
 class PylftpContext:
@@ -13,17 +26,36 @@ class PylftpContext:
     def __init__(self,
                  args,
                  logger: logging.Logger,
-                 web_access_logger: logging.Logger):
+                 web_access_logger: logging.Logger,
+                 config: Config,
+                 patterns: Patterns):
         self.args = args
         self.logger = logger
         self.web_access_logger = web_access_logger
+        self.config = config
+        self.patterns = patterns
 
     def create_child_context(self, context_name: str) -> "PylftpContext":
         return PylftpContext(
             args=self.args,
             logger=self.logger.getChild(context_name),
-            web_access_logger=self.web_access_logger
+            web_access_logger=self.web_access_logger,
+            config=self.config,
+            patterns=self.patterns
         )
+
+    def print_to_log(self):
+        # Print the config
+        self.logger.debug("Config:")
+        for section in self.config.keys():
+            for option in self.config[section].keys():
+                value = self.config[section][option]
+                self.logger.debug("  {}.{}: {}".format(section, option, value))
+
+        # Print the patterns
+        self.logger.debug("Patterns:")
+        for pattern in self.patterns:
+            self.logger.debug("  {}".format(pattern))
 
 
 class PylftpJob(threading.Thread, ABC):
--- src/python/pylftpd.py
+++ src/python/pylftpd.py
@@ -3,12 +3,15 @@
 import signal
 import time
 import logging
-from logging.handlers import RotatingFileHandler
 import sys
 import argparse
+import os
+from logging.handlers import RotatingFileHandler
+from configparser import ConfigParser
 
 # my libs
-from common import PylftpContext
+from common import Config, Patterns
+from common import PylftpError, PylftpContext
 from pylftpmainjob import PylftpMainJob
 from web_app import WebAppJob
 
@@ -35,50 +38,80 @@ class Pylftpd:
     def __init__(self):
         # Parse arguments
         parser = argparse.ArgumentParser(description="PyLFTP daemon")
+        parser.add_argument("-c", "--config", required=True, help="Path to config file")
+        parser.add_argument("-p", "--patterns", required=True, help="Path to patterns file")
         parser.add_argument("--logdir", help="Directory for log files")
-        parser.add_argument("--debug", action="store_true", help="Enable debug logs")
+        parser.add_argument("-d", "--debug", action="store_true", help="Enable debug logs")
         args = parser.parse_args()
 
         # Logger setup
         # We separate the main log from the web-access log
-        logger = logging.getLogger(Pylftpd._SERVICE_NAME)
-        web_access_logger = logging.getLogger(Pylftpd._WEB_ACCESS_LOG_NAME)
-        if args.debug:
-            logger.setLevel(logging.DEBUG)
-            web_access_logger.setLevel(logging.DEBUG)
-        else:
-            logger.setLevel(logging.INFO)
-            web_access_logger.setLevel(logging.INFO)
-        if args.logdir:
-            # Output logs to a file in the given directory
-            handler = RotatingFileHandler(
-                        "{}/{}.log".format(args.logdir, Pylftpd._SERVICE_NAME),
-                        maxBytes=Pylftpd._MAX_LOG_SIZE_IN_BYTES,
-                        backupCount=Pylftpd._LOG_BACKUP_COUNT
-                      )
-            web_access_handler = RotatingFileHandler(
-                                    "{}/{}.log".format(args.logdir, Pylftpd._WEB_ACCESS_LOG_NAME),
-                                    maxBytes=Pylftpd._MAX_LOG_SIZE_IN_BYTES,
-                                    backupCount=Pylftpd._LOG_BACKUP_COUNT
-                                 )
-        else:
-            handler = logging.StreamHandler(sys.stdout)
-            web_access_handler = logging.StreamHandler(sys.stdout)
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
-        handler.setFormatter(formatter)
-        web_access_handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        web_access_logger.addHandler(web_access_handler)
+        logger = self.create_logger(name=Pylftpd._SERVICE_NAME,
+                                    debug=args.debug,
+                                    logdir=args.logdir)
+        web_access_logger = self.create_logger(name=Pylftpd._WEB_ACCESS_LOG_NAME,
+                                               debug=args.debug,
+                                               logdir=args.logdir)
 
+        # Load config
+        config = self.load_config(args.config)
+        # Load patterns
+        patterns = self.load_patterns(args.patterns)
+
+        # Create context
         self.context = PylftpContext(
             args=args,
             logger=logger,
-            web_access_logger=web_access_logger
+            web_access_logger=web_access_logger,
+            config=config,
+            patterns=patterns
         )
 
         # Register the signal handlers
         signal.signal(signal.SIGTERM, self.signal)
         signal.signal(signal.SIGINT, self.signal)
+
+        # Print context to log
+        self.context.print_to_log()
+
+    @staticmethod
+    def create_logger(name: str, debug: bool, logdir: str) -> logging.Logger:
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG if debug else logging.INFO)
+        if logdir is not None:
+            # Output logs to a file in the given directory
+            handler = RotatingFileHandler(
+                        "{}/{}.log".format(logdir, name),
+                        maxBytes=Pylftpd._MAX_LOG_SIZE_IN_BYTES,
+                        backupCount=Pylftpd._LOG_BACKUP_COUNT
+                      )
+        else:
+            handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        return logger
+
+    @staticmethod
+    def load_config(config_file_path: str) -> Config:
+        if not os.path.isfile(config_file_path):
+            raise PylftpError("Config file not found: {}".format(config_file_path))
+        config_parser = ConfigParser()
+        config_parser.read(config_file_path)
+        config = {}
+        for section in config_parser.sections():
+            config[section] = {}
+            for option in config_parser.options(section):
+                config[section][option] = config_parser.get(section, option)
+        return config
+
+    @staticmethod
+    def load_patterns(patterns_file_path: str) -> Patterns:
+        if not os.path.isfile(patterns_file_path):
+            raise PylftpError("Patterns file not found: {}".format(patterns_file_path))
+        with open(patterns_file_path) as f:
+            patterns = [x.strip() for x in f.readlines()]
+        return patterns
 
     def run(self):
         self.context.logger.info("Starting pylftpd")
