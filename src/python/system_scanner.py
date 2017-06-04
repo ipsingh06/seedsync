8bdd540e4dd04890a94d80cfecbe953bbb05a087
Inderpreet Singh <ipsingh06@gmail.com>
2017-06-04 00:37:06 -0400

    System scanner class
    This class scans system for file names and sizes.
    It can be run as a script to grab the file info from the remote size.
    In this usecase, the script must be placed on the remote server and
    run via SSH there.

---
+++ src/python/system_scanner.py
@@ -0,0 +1,128 @@
+# Copyright 2017, Inderpreet Singh, All rights reserved.
+
+import os
+import sys
+import argparse
+import pickle
+from typing import List
+
+
+class SystemFile:
+    """
+    Represents a system file or directory
+    """
+    def __init__(self, name: str, size: int, is_dir: bool = False):
+        if size < 0:
+            raise ValueError("File size must be greater than zero")
+        self.__name = name
+        self.__size = size  # in bytes
+        self.__is_dir = is_dir
+        self.__children = []
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    @property
+    def name(self) -> str: return self.__name
+
+    @property
+    def size(self) -> int: return self.__size
+
+    @property
+    def is_dir(self) -> bool: return self.__is_dir
+
+    @property
+    def children(self) -> List["SystemFile"]: return self.__children
+
+    def add_child(self, file: "SystemFile"):
+        if not self.__is_dir:
+            raise TypeError("Cannot add children to a file")
+        self.__children.append(file)
+
+
+class SystemScanner:
+    """
+    Scans system (local or remote) to generate list of files and sizes
+    """
+    def __init__(self, path_to_scan: str):
+        if not os.path.isdir(path_to_scan):
+            raise ValueError("Path to scan is not a directory: {}".format(path_to_scan))
+        self.path_to_scan = path_to_scan
+        self.exclude_prefixes = []
+
+    def add_exclude_prefix(self, prefix: str):
+        """
+        Exclude files that begin with the given prefix
+        :param prefix:
+        :return:
+        """
+        self.exclude_prefixes.append(prefix)
+
+    def scan(self) -> List[SystemFile]:
+        """
+        Scan the path to generate list of system files
+        The files and its children are inserted in alphabetical order
+        :return:
+        """
+        system_files = []
+
+        def create_children(path: str):
+            children = []
+            for entry in os.scandir(path):
+                # Skip excluded entries
+                skip = False
+                for prefix in self.exclude_prefixes:
+                    if entry.name.startswith(prefix):
+                        skip = True
+                        break
+                if skip:
+                    continue
+
+                if entry.is_dir():
+                    sub_children = create_children(entry.path)
+                    size = sum(sub_child.size for sub_child in sub_children)
+                    sf = SystemFile(entry.name, size, True)
+                    for sub_child in sub_children:
+                        sf.add_child(sub_child)
+                else:
+                    sf = SystemFile(entry.name, entry.stat().st_size, False)
+                children.append(sf)
+            children.sort(key=lambda f: f.name)
+            return children
+
+        return create_children(self.path_to_scan)
+
+
+if __name__ == "__main__":
+    if sys.hexversion < 0x03050000:
+        sys.exit("Python 3.5 or newer is required to run this program.")
+
+    parser = argparse.ArgumentParser(description="File size scanner")
+    parser.add_argument("path", help="Path of the root directory to scan")
+    parser.add_argument("-e", "--exclude-hidden", action="store_true", default=False,
+                        help="Exclude hidden files")
+    parser.add_argument("-H", "--human-readable", action="store_true", default=False,
+                        help="Human readable output")
+    args = parser.parse_args()
+
+    scanner = SystemScanner(args.path)
+    if args.exclude_hidden:
+        scanner.add_exclude_prefix(".")
+    root_files = scanner.scan()
+    if args.human_readable:
+        def print_file(file: SystemFile, level: int):
+            sys.stdout.write("  "*level)
+            sys.stdout.write("{} {} {}\n".format(
+                file.name,
+                "d" if file.is_dir else "f",
+                file.size
+            ))
+            for child in file.children:
+                print_file(child, level+1)
+        for root_file in root_files:
+            print_file(root_file, 0)
+    else:
+        print(pickle.dumps(root_files))
---
+++ src/python/tests/test_system_scanner.py
@@ -0,0 +1,236 @@
+# Copyright 2017, Inderpreet Singh, All rights reserved.
+
+import unittest
+import os
+import shutil
+import tempfile
+
+from system_scanner import SystemFile, SystemScanner
+
+
+class TestSystemFile(unittest.TestCase):
+    def test_name(self):
+        sf = SystemFile("test", 0, False)
+        self.assertEqual("test", sf.name)
+
+    def test_size(self):
+        sf = SystemFile("", 42, False)
+        self.assertEqual(42, sf.size)
+        with self.assertRaises(ValueError) as context:
+            # noinspection PyUnusedLocal
+            sf = SystemFile("", -42, False)
+        self.assertTrue("File size must be greater than zero" in str(context.exception))
+
+    def test_is_dir(self):
+        sf = SystemFile("", 0, True)
+        self.assertEqual(True, sf.is_dir)
+        sf = SystemFile("", 0, False)
+        self.assertEqual(False, sf.is_dir)
+
+    def test_add_child(self):
+        sf = SystemFile("", 0, True)
+        sf.add_child(SystemFile("child1", 42, True))
+        sf.add_child(SystemFile("child2", 99, False))
+        self.assertEqual(2, len(sf.children))
+        self.assertEqual("child1", sf.children[0].name)
+        self.assertEqual(True, sf.children[0].is_dir)
+        self.assertEqual(42, sf.children[0].size)
+        self.assertEqual("child2", sf.children[1].name)
+        self.assertEqual(False, sf.children[1].is_dir)
+        self.assertEqual(99, sf.children[1].size)
+
+    def test_fail_add_child_to_file(self):
+        sf = SystemFile("", 0, False)
+        with self.assertRaises(TypeError) as context:
+            sf.add_child(SystemFile("", 0, False))
+        self.assertTrue("Cannot add children to a file" in str(context.exception))
+
+
+class TestSystemScanner(unittest.TestCase):
+    temp_dir = None
+
+    @classmethod
+    def setUpClass(cls):
+        # Create a temp directory
+        TestSystemScanner.temp_dir = tempfile.mkdtemp(prefix="test_system_scanner")
+
+        # Create a bunch files and directories
+        # a [dir]
+        #   aa [dir]
+        #      .aaa [dir]
+        #      .aab [file, 512 bytes]
+        #   ab [file, 12*1024 + 4 bytes]
+        # b [dir]
+        #   ba [dir]
+        #      baa [file, 512 + 7 bytes]
+        #   bb [dir]
+        #     bba [dir]
+        #     bbb [file, 24*1024*1024 + 24 bytes]
+        #     bbc [dir]
+        #        bbca [dir]
+        #           .bbcaa [file, 1 byte
+        # c [file, 1234 bytes]
+
+        def my_mkdir(*args):
+            os.mkdir(os.path.join(TestSystemScanner.temp_dir, *args))
+
+        def my_touch(size, *args):
+            path = os.path.join(TestSystemScanner.temp_dir, *args)
+            with open(path, 'wb') as f:
+                f.write(bytearray([0xff]*size))
+
+        my_mkdir("a")
+        my_mkdir("a", "aa")
+        my_mkdir("a", "aa", ".aaa")
+        my_touch(512, "a", "aa", ".aab")
+        my_touch(12*1024+4, "a", "ab")
+        my_mkdir("b")
+        my_mkdir("b", "ba")
+        my_touch(512+7, "b", "ba", "baa")
+        my_mkdir("b", "bb")
+        my_mkdir("b", "bb", "bba")
+        my_touch(24*1024*1024+24, "b", "bb", "bbb")
+        my_mkdir("b", "bb", "bbc")
+        # noinspection SpellCheckingInspection
+        my_mkdir("b", "bb", "bbc", "bbca")
+        my_touch(1, "b", "bb", "bbc", "bbca", ".bbcaa")
+        my_touch(1234, "c")
+
+    @classmethod
+    def tearDownClass(cls):
+        # Cleanup
+        shutil.rmtree(TestSystemScanner.temp_dir)
+
+    def test_scan_tree(self):
+        scanner = SystemScanner(TestSystemScanner.temp_dir)
+        files = scanner.scan()
+        self.assertEqual(3, len(files))
+        a, b, c = tuple(files)
+
+        self.assertEqual("a", a.name)
+        self.assertTrue(a.is_dir)
+        self.assertEqual("b", b.name)
+        self.assertTrue(b.is_dir)
+        self.assertEqual("c", c.name)
+        self.assertFalse(c.is_dir)
+
+        self.assertEqual(2, len(a.children))
+        aa, ab = tuple(a.children)
+        self.assertEqual("aa", aa.name)
+        self.assertTrue(aa.is_dir)
+        self.assertEqual(2, len(aa.children))
+        aaa, aab = tuple(aa.children)
+        self.assertEqual(".aaa", aaa.name)
+        self.assertTrue(aaa.is_dir)
+        self.assertEqual(".aab", aab.name)
+        self.assertFalse(aab.is_dir)
+        self.assertEqual("ab", ab.name)
+        self.assertFalse(ab.is_dir)
+
+        self.assertEqual(2, len(b.children))
+        ba, bb = tuple(b.children)
+        self.assertEqual("ba", ba.name)
+        self.assertTrue(ba.is_dir)
+        self.assertEqual(1, len(ba.children))
+        baa = ba.children[0]
+        self.assertEqual("baa", baa.name)
+        self.assertFalse(baa.is_dir)
+        self.assertEqual("bb", bb.name)
+        self.assertTrue(bb.is_dir)
+        self.assertEqual(3, len(bb.children))
+        bba, bbb, bbc = tuple(bb.children)
+        self.assertEqual("bba", bba.name)
+        self.assertTrue(bba.is_dir)
+        self.assertEqual("bbb", bbb.name)
+        self.assertFalse(bbb.is_dir)
+        self.assertEqual("bbc", bbc.name)
+        self.assertTrue(bbc.is_dir)
+        self.assertEqual(1, len(bbc.children))
+        bbca = bbc.children[0]
+        # noinspection SpellCheckingInspection
+        self.assertEqual("bbca", bbca.name)
+        self.assertTrue(bbca.is_dir)
+        self.assertEqual(1, len(bbca.children))
+        bbcaa = bbca.children[0]
+        self.assertEqual(".bbcaa", bbcaa.name)
+        self.assertFalse(bbcaa.is_dir)
+
+    def test_scan_size(self):
+        scanner = SystemScanner(TestSystemScanner.temp_dir)
+        files = scanner.scan()
+        self.assertEqual(3, len(files))
+        a, b, c = tuple(files)
+        aa, ab = tuple(a.children)
+        aaa, aab = tuple(aa.children)
+        ba, bb = tuple(b.children)
+        baa = ba.children[0]
+        bba, bbb, bbc = tuple(bb.children)
+        bbca = bbc.children[0]
+        bbcaa = bbca.children[0]
+
+        self.assertEqual(12*1024+4+512, a.size)
+        self.assertEqual(512, aa.size)
+        self.assertEqual(0, aaa.size)
+        self.assertEqual(512, aab.size)
+        self.assertEqual(12*1024+4, ab.size)
+        self.assertEqual(512+7+24*1024*1024+24+1, b.size)
+        self.assertEqual(512+7, ba.size)
+        self.assertEqual(512+7, baa.size)
+        self.assertEqual(24*1024*1024+24+1, bb.size)
+        self.assertEqual(0, bba.size)
+        self.assertEqual(24*1024*1024+24, bbb.size)
+        self.assertEqual(1, bbc.size)
+        self.assertEqual(1, bbca.size)
+        self.assertEqual(1, bbcaa.size)
+        self.assertEqual(1234, c.size)
+
+    def test_scan_tree_excluded(self):
+        scanner = SystemScanner(TestSystemScanner.temp_dir)
+        scanner.add_exclude_prefix(".")
+        files = scanner.scan()
+        self.assertEqual(3, len(files))
+        a, b, c = tuple(files)
+        aa, ab = tuple(a.children)
+        ba, bb = tuple(b.children)
+        bba, bbb, bbc = tuple(bb.children)
+        bbca = bbc.children[0]
+        self.assertEqual(0, len(aa.children))
+        self.assertEqual(0, len(bbca.children))
+
+        scanner.add_exclude_prefix("ab")
+        files = scanner.scan()
+        self.assertEqual(3, len(files))
+        a, b, c = tuple(files)
+        self.assertEqual(1, len(a.children))
+        aa = a.children[0]
+        ba, bb = tuple(b.children)
+        bba, bbb, bbc = tuple(bb.children)
+        bbca = bbc.children[0]
+        self.assertEqual("aa", aa.name)
+        self.assertEqual(0, len(bbca.children))
+
+    def test_scan_size_excluded(self):
+        scanner = SystemScanner(TestSystemScanner.temp_dir)
+        scanner.add_exclude_prefix(".")
+        files = scanner.scan()
+        self.assertEqual(3, len(files))
+        a, b, c = tuple(files)
+        aa, ab = tuple(a.children)
+        ba, bb = tuple(b.children)
+        bba, bbb, bbc = tuple(bb.children)
+        bbca = bbc.children[0]
+        self.assertEqual(12*1024+4, a.size)
+        self.assertEqual(0, aa.size)
+        self.assertEqual(24*1024*1024+24+0, bb.size)
+        self.assertEqual(0, bbc.size)
+        self.assertEqual(0, bbca.size)
+
+        scanner.add_exclude_prefix("ab")
+        files = scanner.scan()
+        self.assertEqual(3, len(files))
+        a, b, c = tuple(files)
+        self.assertEqual(1, len(a.children))
+        aa = a.children[0]
+        self.assertEqual("aa", aa.name)
+        self.assertEqual(0, a.size)
+        self.assertEqual(0, aa.size)
