bbda69413bacd406adef719e7618e588a2b57a17
Inderpreet Singh <ipsingh06@gmail.com>
2017-09-24 22:34:54 -0700

    Controller: dl'ing files input in model builder

--- src/python/controller/model_builder.py
+++ src/python/controller/model_builder.py
@@ -14,18 +14,23 @@ class ModelBuilder:
     """
     ModelBuilder combines all the difference sources of file system info
     to build a model. These sources include:
+      * downloading file system as a Dict[name, SystemFile]
       * local file system as a Dict[name, SystemFile]
       * remote file system as a Dict[name, SystemFile]
       * lftp status as Dict[name, LftpJobStatus]
     """
     def __init__(self):
         self.logger = logging.getLogger("ModelBuilder")
+        self.__downloading_files = dict()
         self.__local_files = dict()
         self.__remote_files = dict()
         self.__lftp_statuses = dict()
 
     def set_base_logger(self, base_logger: logging.Logger):
         self.logger = base_logger.getChild("ModelBuilder")
+
+    def set_downloading_files(self, downloading_files: List[SystemFile]):
+        self.__downloading_files = {file.name: file for file in downloading_files}
 
     def set_local_files(self, local_files: List[SystemFile]):
         self.__local_files = {file.name: file for file in local_files}
@@ -49,7 +54,8 @@ class ModelBuilder:
                                      self.__lftp_statuses.keys())
         for name in all_file_names:
             remote = self.__remote_files.get(name, None)
-            local = self.__local_files.get(name, None)
+            # use downloading file if it exists, otherwise local
+            local = self.__downloading_files.get(name, None) or self.__local_files.get(name, None)
             status = self.__lftp_statuses.get(name, None)
 
             if remote is None and local is None and status is None:
@@ -73,12 +79,8 @@ class ModelBuilder:
                 if _local:
                     _model_file.local_size = _local.size
 
-                # overwrite sizes with status info
-                if _transfer_state:
-                    if _transfer_state.size_local:
-                        _model_file.local_size = _transfer_state.size_local
-                    if _transfer_state.size_remote:
-                        _model_file.remote_size = _transfer_state.size_remote
+                # Note: no longer use lftp's file sizes
+                #       they represent remaining size for resumed downloads
 
                 # set the downloading speed and eta
                 if _transfer_state:
