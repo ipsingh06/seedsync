ca50265cb75ccc0d09d8b621a06ec666c6a326e4
Inderpreet Singh <ipsingh06@gmail.com>
2017-10-13 20:13:06 -0700

    Fix unittests

--- src/python/tests/unittests/test_pylftpd.py
+++ src/python/tests/unittests/test_pylftpd.py
@@ -22,33 +22,50 @@ class TestPylftpd(unittest.TestCase):
     def test_args_config(self):
         sys.argv.append("-c")
         sys.argv.append("/path/to/config")
+        sys.argv.append("--html")
+        sys.argv.append("/path/to/html")
         args = Pylftpd._parse_args()
         self.assertIsNotNone(args)
         self.assertEqual("/path/to/config", args.config_dir)
 
-        sys.argv = sys.argv[:-2]
+        sys.argv = sys.argv[:-4]
         sys.argv.append("--config_dir")
         sys.argv.append("/path/to/config")
+        sys.argv.append("--html")
+        sys.argv.append("/path/to/html")
         args = Pylftpd._parse_args()
         self.assertIsNotNone(args)
         self.assertEqual("/path/to/config", args.config_dir)
 
-        sys.argv = sys.argv[:-2]
+        sys.argv = sys.argv[:-4]
         with self.assertRaises(SystemExit):
             Pylftpd._parse_args()
+
+    def test_args_html(self):
+        sys.argv.append("-c")
+        sys.argv.append("/path/to/config")
+        sys.argv.append("--html")
+        sys.argv.append("/path/to/html")
+        args = Pylftpd._parse_args()
+        self.assertIsNotNone(args)
+        self.assertEqual("/path/to/html", args.html)
 
     def test_args_logdir(self):
         sys.argv.append("-c")
         sys.argv.append("/path/to/config")
         sys.argv.append("--logdir")
         sys.argv.append("/path/to/logdir")
+        sys.argv.append("--html")
+        sys.argv.append("/path/to/html")
         args = Pylftpd._parse_args()
         self.assertIsNotNone(args)
         self.assertEqual("/path/to/logdir", args.logdir)
 
-        sys.argv = sys.argv[:-4]
+        sys.argv = sys.argv[:-6]
         sys.argv.append("-c")
         sys.argv.append("/path/to/config")
+        sys.argv.append("--html")
+        sys.argv.append("/path/to/html")
         args = Pylftpd._parse_args()
         self.assertIsNotNone(args)
         self.assertIsNone(args.logdir)
@@ -56,22 +73,28 @@ class TestPylftpd(unittest.TestCase):
     def test_args_debug(self):
         sys.argv.append("-c")
         sys.argv.append("/path/to/config")
+        sys.argv.append("--html")
+        sys.argv.append("/path/to/html")
         sys.argv.append("-d")
         args = Pylftpd._parse_args()
         self.assertIsNotNone(args)
         self.assertTrue(args.debug)
 
-        sys.argv = sys.argv[:-3]
+        sys.argv = sys.argv[:-5]
         sys.argv.append("-c")
         sys.argv.append("/path/to/config")
         sys.argv.append("--debug")
+        sys.argv.append("--html")
+        sys.argv.append("/path/to/html")
         args = Pylftpd._parse_args()
         self.assertIsNotNone(args)
         self.assertTrue(args.debug)
 
-        sys.argv = sys.argv[:-3]
+        sys.argv = sys.argv[:-5]
         sys.argv.append("-c")
         sys.argv.append("/path/to/config")
+        sys.argv.append("--html")
+        sys.argv.append("/path/to/html")
         args = Pylftpd._parse_args()
         self.assertIsNotNone(args)
         self.assertFalse(args.debug)
