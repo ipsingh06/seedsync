ecf97d7632950a7555a0c37b3c0e94eb2a00c6cc
Inderpreet Singh <ipsingh06@gmail.com>
2018-03-08 02:36:28 -0800

    Updated version to 0.4-0

--- src/e2e/tests/about.page.spec.ts
+++ src/e2e/tests/about.page.spec.ts
@@ -13,6 +13,6 @@ describe('Testing about page', () => {
     });
 
     it('should have the right version', () => {
-        expect(page.getVersion()).toEqual("v0.3.0");
+        expect(page.getVersion()).toEqual("v0.4.0");
     });
 });
