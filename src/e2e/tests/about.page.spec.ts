a0f468cc2e80903e364ff8466929002748d01f3a
Inderpreet Singh <ipsingh06@gmail.com>
2018-03-21 23:11:18 -0700

    Updated version to v0.6-0

--- src/e2e/tests/about.page.spec.ts
+++ src/e2e/tests/about.page.spec.ts
@@ -13,6 +13,6 @@ describe('Testing about page', () => {
     });
 
     it('should have the right version', () => {
-        expect(page.getVersion()).toEqual("v0.5.1");
+        expect(page.getVersion()).toEqual("v0.6.0");
     });
 });
